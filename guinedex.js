6813e8130641ca1056a57e3b79f00df78dad0402
guinedex <ferretmage@disroot.org>
2026-07-20 23:45:37 -0400

    Lots of templates, almost ready to start adding js

--- guinedex.js
+++ guinedex.js
@@ -1,6 +1,35 @@
+let skins = [];
+
+function setOptionsVisible(visible) {
+	if (visible) {
+		$("#options").removeClass("d-none");
+		$("#skin-info").addClass("d-none");
+		$("#options-btn-arrow").text("[<]");
+	} else {
+		$("#options").addClass("d-none");
+		$("#skin-info").removeClass("d-none");
+		$("#options-btn-arrow").text("[>]");
+	}
+}
+function toggleOptionsVisible() {
+	setOptionsVisible($("#options").hasClass("d-none"))
+}
+
+function buildBaseSkinPreview(skin_id) {
+	return $("<p>test</p>");
+}
+
 $(window).on("load", () => {
-    console.log("Loaded");
+    console.log("Page loaded");
+
 	$.getJSON("dex.json", (data) => {
-		console.log(data);
+		skins = data;
+		console.log("Fetched skins");
+
+		buildBaseSkinPreview(0).appendTo($("#skin-list"));
+	});
+
+	$("#options-btn").on("click", (e) => {
+		toggleOptionsVisible();
 	});
 });
